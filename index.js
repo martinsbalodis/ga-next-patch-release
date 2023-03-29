454a8f1e2389a42182c9b6ddaf574e20ef038f50
Einars Ozols <einarsozolsozols@gmail.com>
2023-03-29 14:26:43 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const core = require("@actions/core");
+const Action = require("./action");
+const github = require("@actions/github");
+
+async function exec() {
+    try {
+        const githubToken = core.getInput("github_token");
+        const baseRelease = core.getInput("base_release", {required: true});
+        const githubConfig = {
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            token: githubToken,
+        };
+
+        const nextRelease = await new Action(githubConfig, baseRelease).getNextPatchRelease();
+        core.setOutput("next_release", nextRelease);
+    } catch (error) {
+        core.setFailed(error);
+    }
+}
+
+exec();
